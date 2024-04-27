242b1f27510462b33844743c885da8a23da3b8f9
Grisha Michurov <michurov.grischa@yandex.ru>
2024-04-27 11:53:54 +0200

    первые два задания

---
+++ task.js
@@ -0,0 +1,21 @@
+// Задание "Переменные"
+let Name = 'Джон';
+let Admin
+Admin = Name
+alert(Admin)
+
+
+let palnetEarth = 'Земля'
+let currentUserName = 'Тарас'
+
+const BIRTHDAY = '18.04.1982'; // BIRTHDAY постоянная переменная, можно капсом
+const age = someCode(BIRTHDAY); // age изменяемая
+
+// Задание "Типы данных"
+let name = "Ilya";
+
+alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name}` ); // hello Ilya
