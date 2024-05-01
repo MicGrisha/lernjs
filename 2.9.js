30c072215541849617fec1ec7838a816a088448c
Grisha Michurov <michurov.grischa@yandex.ru>
2024-05-01 14:15:29 +0200

    Задание Операторы сравнения

---
+++ 2.9.js
@@ -0,0 +1,7 @@
+5 > 4 = true
+"ананас" > "яблоко" = false // а>я
+"2" > "12" = false //true правильный сравнеие по символам
+undefined == null = true // спец случай
+undefined === null = false // разные типы
+null == "\n0\n" = false 
+null === +"\n0\n" = false //разные типы
